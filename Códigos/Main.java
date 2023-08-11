b834c030b416134dc512dc62bf5bfee339ca9530
PauloVictorRangel <104431068+PauloVictorRangel@users.noreply.github.com>
2023-08-10 23:27:05 -0300

    Add files via upload

---
+++ Códigos/Main.java
@@ -0,0 +1,12 @@
+public class MyClass{
+    public static void main(String[] args){
+
+        Veiculo BMW = new Veiculo();
+        Cliente Corte = new Cliente();
+        Seguro Protecaototal = new Seguro();
+
+        Corte.nome = "Corte";
+        Corte.cpf = "5461297346";
+
+    }
+}
