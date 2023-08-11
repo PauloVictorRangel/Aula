b289ed17151e6896ad33991627e299de49150f4b
PauloVictorRangel <104431068+PauloVictorRangel@users.noreply.github.com>
2023-08-10 23:26:42 -0300

    Delete Aula Modelagem CARRO/Códigos directory

--- Aula Modelagem CARRO/Códigos/Modelo.java
+++
@@ -1,3 +0,0 @@
-public Modelo{
-    private String name;
-}
--- Aula Modelagem CARRO/Códigos/Veiculo.java
+++
@@ -1,9 +0,0 @@
-public class Veiculo{
-    private int ano;
-    private String marca;
-    private String cor;
-    private String placa;
-
-    //ligação 1 pra 1 entre modelo e veiculo
-    private Modelo modelo;
-}
